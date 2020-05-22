9aa640e9d7558956bf6db5f8b2c177447b117e5c
unknown <druharper@gmail.com>
2020-05-22 16:16:57 -0500

    just gettin' started...

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,5 +1,24 @@
 function main() {
-  // Put your code here
+    // Put your code here
+
+    console.log("My ENEMIES LIST")
+    console.log("===============")
+
+    // for (name of names) {
+    //   console.log(name)
+    // }
+
+    const enemies = getEnemies;
+
+    var person = new Object();
+    person.firstName = "John";
+    person.lastName = "Doe";
+    person.age = 50;
+    person.eyeColor = "blue";
+
+
+
+
 
 }
 
--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,5 +1,27 @@
 function main() {
-  // Put your code here
+    // Put your code here
+    console.log("Duuuuuuude!")
+
+    const names = ["Nashville", "Hong Kong", "The back yard", "Earth",
+        "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+    ]
+
+    // Print some output to the terminal (aka. "Console");
+    console.log("All Place Names");
+    for (name of names) {
+        console.log(name)
+    }
+
+    console.log();
+
+    console.log("All The The Names");
+    for (name of names) {
+        if (name.startsWith("The")) {
+            console.log(name)
+        }
+    }
+
+
 
 }
 
