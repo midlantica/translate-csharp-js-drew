b1bd48a63254a5d99cd755ca35d0eb3001e36c39
Andrew Harper <druharper@gmail.com>
2020-05-24 12:59:11 -0500

    back to mac

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,53 +1,54 @@
-function main() {
-    console.log('My ENEMIES LIST')
-    console.log('===============')
+const main = () => {
+  console.log('My ENEMIES LIST')
+  console.log('===============')
 
-    const enemies = [{
-            FirstName: 'Joshua',
-            LastName: 'Flowers',
-            Offenses: [
-                'Being a jerk to me in elementary school',
-                'Not being nice to me in elementary school',
-            ],
-            IsReallyHated: true,
-        },
-        {
-            FirstName: 'Darth',
-            LastName: 'Vader',
-            Offenses: [
-                "Cut off Luke's hand",
-                'Murdered all those kids',
-                'Unkind management practices',
-            ],
-            IsReallyHated: true,
-        },
-        {
-            FirstName: 'Betty',
-            LastName: 'Rudelady',
-            Offenses: [
-                'Phone calls in the theater',
-                'Phone calls on the bus',
-                'Phone calls in line at the grocery store',
-                'Poor conversationalist',
-            ],
-            IsReallyHated: true,
-        },
-        {
-            FirstName: 'Leon',
-            LastName: 'Peck',
-            Offenses: ['Keeps giving me a hotplate'],
-            IsReallyHated: false,
-        },
-    ]
+  const enemies = [
+    {
+      FirstName: 'Joshua',
+      LastName: 'Flowers',
+      Offenses: [
+        'Being a jerk to me in elementary school',
+        'Not being nice to me in elementary school',
+      ],
+      IsReallyHated: true,
+    },
+    {
+      FirstName: 'Darth',
+      LastName: 'Vader',
+      Offenses: [
+        "Cut off Luke's hand",
+        'Murdered all those kids',
+        'Unkind management practices',
+      ],
+      IsReallyHated: true,
+    },
+    {
+      FirstName: 'Betty',
+      LastName: 'Rudelady',
+      Offenses: [
+        'Phone calls in the theater',
+        'Phone calls on the bus',
+        'Phone calls in line at the grocery store',
+        'Poor conversationalist',
+      ],
+      IsReallyHated: true,
+    },
+    {
+      FirstName: 'Leon',
+      LastName: 'Peck',
+      Offenses: ['Keeps giving me a hotplate'],
+      IsReallyHated: false,
+    },
+  ]
 
-    for (myEnemy in enemies) {
-        const first = enemies[myEnemy].FirstName
-        const last = enemies[myEnemy].LastName
-        const fullName = `${first} ${last}`
-        if (enemies[myEnemy].IsReallyHated) {
-            console.log(fullName)
-        }
+  for (myEnemy in enemies) {
+    const first = enemies[myEnemy].FirstName
+    const last = enemies[myEnemy].LastName
+    const fullName = `${first} ${last}`
+    if (enemies[myEnemy].IsReallyHated) {
+      console.log(fullName)
     }
+  }
 }
 
 main()
--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,28 +1,30 @@
 function main() {
-    // Put your code here
-    console.log("Duuuuuuude!")
+  // Put your code here
+  const names = [
+    'Nashville',
+    'Hong Kong',
+    'The back yard',
+    'Earth',
+    'London',
+    'The mall',
+    'Ryman Auditorium',
+    'The Great Wall of China',
+  ]
 
-    const names = ["Nashville", "Hong Kong", "The back yard", "Earth",
-        "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
-    ]
+  // Print some output to the terminal (aka. "Console");
+  console.log('All Place Names')
+  for (name of names) {
+    console.log(name)
+  }
 
-    // Print some output to the terminal (aka. "Console");
-    console.log("All Place Names");
-    for (name of names) {
-        console.log(name)
+  console.log()
+
+  console.log("All the 'The' Names")
+  for (name of names) {
+    if (name.startsWith('The')) {
+      console.log(name)
     }
-
-    console.log();
-
-    console.log("All the 'The' Names");
-    for (name of names) {
-        if (name.startsWith("The")) {
-            console.log(name)
-        }
-    }
-
-
-
+  }
 }
 
-main();
+main()
